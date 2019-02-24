2c14d98df4458c5d505f75803af2f39d5869944f
José <joseadilsson@gmail.com>
2019-02-24 15:55:33 -0300

    Iniciado arquivode routes e login.

---
+++ mobile/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import { View, StyleSheet } from 'react-native'
+
+export default class Login extends Component {
+  render() {
+    return (
+      <View style={styles.container} />
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF"
+  },
+});
---
+++ mobile/src/routes.js
@@ -0,0 +1,11 @@
+import { createSwitchNavigator, createAppContainer } from 'react-navigation'
+
+import Login from './pages/Login'
+
+const Routes = createAppContainer(
+  createSwitchNavigator({
+    Login
+  })
+);
+
+export default Routes
